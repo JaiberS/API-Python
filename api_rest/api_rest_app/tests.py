b52c22fc50f44fee8993e94729540be9c180d439
Jaiber Ramirez <jramirez@masiv.com>
2024-07-07 14:04:17 -0500

    added tests with authentication

--- api_rest/api_rest_app/tests.py
+++ api_rest/api_rest_app/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Book
+from datetime import date
+from django.urls import reverse
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class BookListViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_view_url_exists_at_desired_location(self):
+        resp = self.client.get('/api/books/')
+        self.assertEqual(resp.status_code, 200)
+
+class BookModelTest(TestCase):
+    def test_string_representation(self):
+        book = Book(title="1984", author="George Orwell", publication=date.today())
+        self.assertEqual(str(book), book.title)
+
+    def test_book_creation(self):
+        book = Book.objects.create(
+            title="1984",
+            author="George Orwell",
+            publication=date(1949, 6, 8)
+        )
+        self.assertTrue(isinstance(book, Book))
+        self.assertEqual(book.__str__(), book.title)
